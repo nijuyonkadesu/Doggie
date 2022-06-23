0ce1140425de8e21f45a4f9f06361ac284fbc285
nijuyonkadesu <24ksriramv@gmail.com>
2022-06-23 08:54:03 +0530

    Added tests

--- app/src/main/java/com/njk/doggie/data/Datasource.kt
+++ app/src/main/java/com/njk/doggie/data/Datasource.kt
@@ -8,10 +8,10 @@ class Datasource {
         return listOf<Dog>(
             Dog(R.drawable.tzeitel, R.string.d1, R.string.a1, R.string.h1),
             Dog(R.drawable.leroy, R.string.d2, R.string.a2, R.string.h2),
-            Dog(R.drawable.nox, R.string.d3, R.string.a3, R.string.h3),
+            Dog(R.drawable.frankie, R.string.d3, R.string.a3, R.string.h3),
+            Dog(R.drawable.nox, R.string.d6, R.string.a6, R.string.h6),
             Dog(R.drawable.faye, R.string.d4, R.string.a4, R.string.h4),
-            Dog(R.drawable.bella, R.string.d5, R.string.a5, R.string.h5),
-            Dog(R.drawable.frankie, R.string.d6, R.string.a6, R.string.h6),
+            Dog(R.drawable.bella, R.string.d5, R.string.a5, R.string.h5)
         )
     }
 }
